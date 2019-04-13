162ae8a6afe4da2ca97414b16acb59da2f34ccd7
Paula Tulis <tulis.paula@gmail.com>
2019-04-13 16:32:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,16 +1,13 @@
 def my_collect(array)
   if block_given?
-    my_new_collection = []
     i = 0
+    my_new_collection = []
  
     while i < array.length
-      yield(array[i])
+     my_new_collection << yield(array[i])
       i = i + 1
-      my_new_collection << array
     end
  
     my_new_collection
 end
-end 
-
 
