c2d26656aa5733e036db1a238a04c826d2172324
Paula Tulis <tulis.paula@gmail.com>
2019-04-13 16:33:01 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    my_new_collection = []
+ 
+    while i < array.length
+     my_new_collection << yield(array[i])
+      i = i + 1
+    end
+ 
+    my_new_collection
+end
+end 
 
